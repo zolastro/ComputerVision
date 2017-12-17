402debae80f1f18f71b675085e4fc06e39485045
David Ponce <zolastrodavid@gmail.com>
2017-12-17 23:28:59 +0100

    Bottles classified using covariance matrices & Hu moments

--- Practise6_BayesianClassifier/Exercise1.m
+++ Practise6_BayesianClassifier/Exercise1.m
@@ -4,8 +4,15 @@ symbols = '*xc';
 colors = 'brg';
 momentos = zeros(15, 7);
 centroides = zeros(7, 1);
-covariance_matrix = zeros(7, 7);
+matriz_covarianza = zeros(7, 7);
 momentos_centrados = zeros(15, 7);
+
+%Used to classify bottles
+global centroides_clases;
+global matrices_covarianza_clases;
+centroides_clases = zeros(7, 3);
+matrices_covarianza_clases = zeros(7, 7, 3);
+
 for c = 1: 3
     for i = 1: 15
         im = imread(strcat(strcat('botella_', categories(c), '_', int2str(i)), '.bmp'));
@@ -16,10 +23,24 @@ for c = 1: 3
        centroides(i) = sum(momentos(:, i))./numero_imagenes;
        momentos_centrados(:, i) = momentos(:, i) - centroides(i);
     end
-   covariance_matrix = (momentos_centrados' * momentos_centrados) ./ numero_imagenes;
-   error_ellipse(covariance_matrix(1:2, 1:2) .* 10, centroides(1:2) ), hold on;
+   centroides_clases(:, c) = centroides(:);
+   matriz_covarianza = (momentos_centrados' * momentos_centrados) ./ numero_imagenes;
+   matrices_covarianza_clases(:, :, c) = matriz_covarianza(:, :);
+   error_ellipse(matriz_covarianza(1:2, 1:2) .* 10, centroides(1:2) ), hold on;
    scatter(momentos(:, 1), momentos(:, 2), symbols(c), colors(c));
    scatter(centroides(1), centroides(2), 's', 'r');
 end
 hold off;
+
+disp("Classifying some bottles...");
+
+for c = 1:3
+    for i = 11: 15
+        name = strcat(strcat('botella_', categories(c), '_', int2str(i)), '.bmp');
+        im = imread(name);
+        disp(strcat("Bottle ", name,  " belongs to: ", categories(belonging_class(im))));
+    end
+end
+
+
 
