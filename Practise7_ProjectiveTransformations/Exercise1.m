038586dcbced150e8d57e7757568393a870cd166
David Ponce <zolastrodavid@gmail.com>
2018-01-13 15:23:23 +0100

    Removes the perspective of a plane in an image

---
+++ Practise7_ProjectiveTransformations/Exercise1.m
@@ -0,0 +1,22 @@
+path = input('Introduce the name of the image: ');
+im = imread(path);
+im = rgb2gray(im);
+im = im2double(im);
+imshow(im);
+p = ginput(4);
+p_h = [p ones(4, 1)]';
+disp(p_h);
+
+scale = 1;
+
+width = 69 * scale;
+height = 105 * scale;
+
+obj_h = [1, 1, 1; width, 1, 1; width, height, 1; 1, height, 1]';
+disp(obj_h)
+H = homography2d(p_h, obj_h);
+
+tf = maketform('projective', H');
+newIm = imtransform(im, tf, 'Xdata', [1 width], 'yData', [1 height]);
+figure(2)
+imshow(newIm);
